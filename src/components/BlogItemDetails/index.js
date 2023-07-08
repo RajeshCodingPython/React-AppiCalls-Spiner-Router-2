68c2f09e98fce526380db5c70c8b89ddd066a423
rajesh <rajesh123@gmail.com>
2023-07-08 19:39:15 +0000

    first commit

--- src/components/BlogItemDetails/index.js
+++ src/components/BlogItemDetails/index.js
@@ -1,30 +1,54 @@
+import Loader from 'react-loader-spinner'
+
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+
 import {Component} from 'react'
 
 import './index.css'
 
-const blogData = {
-  title: 'Blog Name',
-  imageUrl: 'https://assets.ccbp.in/frontend/react-js/placeholder-3-img.png',
-  avatarUrl: 'https://assets.ccbp.in/frontend/react-js/avatar-img.png',
-  author: 'Author Name',
-  content:
-    'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-}
-
 class BlogItemDetails extends Component {
+  state = {blogData: {}, isLoded: true}
+
+  componentDidMount = () => {
+    this.getBlogItemData()
+  }
+
+  getBlogItemData = async () => {
+    const {match} = this.props
+    const {id} = match.params
+    const response = await fetch(`https://apis.ccbp.in/blogs/${id}`)
+    const data = await response.json()
+    const updateData = {
+      title: data.data,
+      imageUrl: data.image_url,
+      avatarUrl: data.avatar_url,
+      author: data.author,
+      content: data.content,
+    }
+    this.setState({blogData: updateData, isLoded: false})
+  }
+
   renderBlogItemDetails = () => {
+    const {blogData, isLoded} = this.state
+
     const {title, imageUrl, content, avatarUrl, author} = blogData
     return (
       <div className="blog-info">
-        <h2 className="blog-details-title">{title}</h2>
+        {isLoded ? (
+          <Loader type="TailSpin" color="#00ffff" height={50} width={50} />
+        ) : (
+          <>
+            <h2 className="blog-details-title">{title}</h2>
 
-        <div className="author-details">
-          <img className="author-pic" src={avatarUrl} alt={author} />
-          <p className="details-author-name">{author}</p>
-        </div>
+            <div className="author-details">
+              <img className="author-pic" src={avatarUrl} alt={author} />
+              <p className="details-author-name">{author}</p>
+            </div>
 
-        <img className="blog-image" src={imageUrl} alt={title} />
-        <p className="blog-content">{content}</p>
+            <img className="blog-image" src={imageUrl} alt={title} />
+            <p className="blog-content">{content}</p>
+          </>
+        )}
       </div>
     )
   }
